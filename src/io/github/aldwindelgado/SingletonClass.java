4c53bd2721f7d90fb7004b40e5d426b4a5daf66a
Aldwin Delgado <aldwinpaolodelgado@gmail.com>
2020-04-08 20:13:35 +0800

    Made the class thread-safe

--- src/io/github/aldwindelgado/SingletonClass.java
+++ src/io/github/aldwindelgado/SingletonClass.java
@@ -5,19 +5,33 @@ package io.github.aldwindelgado;
  */
 public class SingletonClass {
 
-    private static SingletonClass instance = null;
+    /*  volatile = new to Java; marks this class to remain a singleton all throughout the duration
+        this app is running
+     */
+    private static volatile SingletonClass instance = null;
 
     // avoid people to instantiating the class outside the created method 'getInstance()'
     private SingletonClass() {
+        // avoid this class to be implemented using Reflections
+        if (instance != null) {
+            throw new RuntimeException("Use 'getInstance()' method to instantiate");
+        }
     }
 
     // return the instantiated class
     public static SingletonClass getInstance() {
         /*  make the singleton implementation lazily loaded by instantiating the class
-            IF and ONLY IF the 'instance' variable IS NULL
+            IF and ONLY IF the 'instance' variable IS NULL and not instantiated anywhere else
          */
         if (instance == null) {
-            instance = new SingletonClass();
+            /* marks this class thread-safe and will not instantiate another SingletonClass
+               once it created one even if two threads are racing to instantiate this class
+            */
+            synchronized (SingletonClass.class) {
+                if (instance == null) {
+                    instance = new SingletonClass();
+                }
+            }
         }
 
         return instance;
